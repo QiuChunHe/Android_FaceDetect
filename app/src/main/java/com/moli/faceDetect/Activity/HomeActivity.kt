8c2b94a8a8fbdb5b9f362b2d21bff46af8eb1932
chunqiu <1468435341@qq.com>
2024-10-28 13:48:59 +0800

    增加基础Activity、Home

---
+++ app/src/main/java/com/moli/faceDetect/Activity/HomeActivity.kt
@@ -0,0 +1,25 @@
+package com.moli.faceDetect.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.moli.faceDetect.R
+
+class HomeActivity : BaseActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun afterRequestPermission(requestCode: Int, isAllGranted: Boolean) {
+        TODO("Not yet implemented")
+    }
+}
